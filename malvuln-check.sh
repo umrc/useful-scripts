33fa52c3f4b5f3b9a89b2e4d4fed80007ac6f4ef
umrc <63246039+umrc@users.noreply.github.com>
2021-02-01 13:27:09 +0100

    Create malvuln-check.sh

---
+++ malvuln-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+if [ "$1" != "" ]; then
+    echo
+    echo "The md5 has of the file is: $(md5sum $1)"
+    echo
+    echo "Downloading the malware report if exist"
+    echo
+    wget http://malvuln.com/advisory/$(md5sum $1).txt
+    else
+        while read -p "Enter the file path:" input
+        do
+            printf -v input "$(eval echo "$input")"
+            if [ "$input" != "" ]; then
+                echo
+                echo "The md5 has of the file is: $(md5sum $input)"
+                echo
+                echo "Downloading the malware report if exist"
+                echo
+                wget http://malvuln.com/advisory/$(md5sum $input).txt
+                break
+            else
+                echo 'Please provide the file path!'
+                continue
+            fi
+        done
+fi
